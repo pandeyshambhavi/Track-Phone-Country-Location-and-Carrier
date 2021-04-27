8c1551b465c84f18695fd07d95008b018fed8470
Shambhavi Pandey <83297985+pandeyshambhavi@users.noreply.github.com>
2021-04-27 22:09:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 27 20:41:47 2021
+
+@author: SHAMBHAVI
+"""
+
+import phonenumbers
+from testt import number
+
+from phonenumbers import geocoder
+ch_nmber = phonenumbers.parse(number, "CH")
+print (geocoder.description_for_number(ch_nmber, "en"))
+
+from phonenumbers import carrier
+service_nmber= phonenumbers.parse(number, "RO")
+print (carrier.name_for_number(service_nmber, "en"))
